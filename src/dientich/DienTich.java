887fd89c62e9d149e9155fba0854ca3ed08eee9b
0TinNguyen0 <trunga92loctri@gmail.com>
2021-09-08 15:21:56 +0700

    init project

--- src/dientich/DienTich.java
+++ src/dientich/DienTich.java
@@ -1,13 +1,13 @@
 
 package dientich;
-
+   import java.util.Scanner;
 public class DienTich {
     public static void main(String[] args) {
-        double radius;
-        double area;
-        radius = 20;
-        area = radius * radius * 3.14159;
-        System.out.println("The area for the circle of radius" + radius + " is " + area);
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter a double value: ");
+        double radius = input.nextDouble();
+        double area = radius*radius*3.14159;
+        System.out.println("The area for the circle of radius is " + area);
     }
     
 }
